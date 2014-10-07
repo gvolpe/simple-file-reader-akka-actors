e85ecc1e18dea68be65f4bab5f11fc218b620b89
Gabi Volpe <gvolpe@dridco.com>
2014-10-07 15:50:22 -0300

    Se ordena clase debajo del companion object.

--- src/main/scala/com/gvolpe/akka/actors/WordCounterActor.scala
+++ src/main/scala/com/gvolpe/akka/actors/WordCounterActor.scala
@@ -9,46 +9,41 @@ object WordCounterActor {
   def props(filename: String) = {
     Props(new WordCounterActor(filename))
   }
-  
-  class WordCounterActor(filename: String) extends Actor {
-  
-    private var totalLines = 0
-    private var linesProcessed = 0
-    private var result = 0
-    private var fileSender: Option[ActorRef] = None
-    
-    def receive = startProcess
-    
-    def processAlreadyRunning: Receive = {
-      case StartProcessFileMsg() => {
-        println("Process is already running!") 
-      }
-      case StringProcessedMsg(wordsAmount) => {
-        result += wordsAmount
-        linesProcessed += 1
-        if (linesProcessed == totalLines) {
-          fileSender.map(_ ! result)
-        }
-      }
-      case _ => println("WordCounterActor: Message not recognized.")
-    }
-    
-    def startProcess: Receive = {
-      
-      case StartProcessFileMsg() => {
-    	  context.become(processAlreadyRunning)
-        fileSender = Some(sender)
-        
-        import scala.io.Source._
-        fromFile(filename).getLines.foreach { line =>
-          context.actorOf(Props[StringCounterActor]) ! ProcessStringMsg(line)
-          totalLines += 1
-        }
-      }
-      case _ => println("WordCounterActor: Message not recognized.")
-      
-    }
-    
-  }
 
+}
+
+class WordCounterActor(filename: String) extends Actor {
+  
+  private var totalLines = 0
+  private var linesProcessed = 0
+  private var result = 0
+  private var fileSender: Option[ActorRef] = None
+  
+  def receive = startProcess
+  
+  def processAlreadyRunning: Receive = {
+    case StringProcessedMsg(wordsAmount) => {
+      result += wordsAmount
+      linesProcessed += 1
+      if (linesProcessed == totalLines) {
+        fileSender.map(_ ! result)
+      }
+    }
+    case _ => println("Process is already running!")
+  }
+  
+  def startProcess: Receive = {
+    case StartProcessFileMsg() => {
+      context.become(processAlreadyRunning)
+      fileSender = Some(sender)
+      
+      import scala.io.Source._
+      fromFile(filename).getLines.foreach { line =>
+        context.actorOf(Props[StringCounterActor]) ! ProcessStringMsg(line)
+        totalLines += 1
+      }
+    }
+    case _ => println("WordCounterActor: Message not recognized.")
+  }
+  
 }
